d68459caeefe23be895ea25a286d6e748094c9ff
Tushar <tusharm@inneed.cloud>
2021-06-28 19:43:18 +0600

    margin remove

--- frontend/src/components/ServiceOrderList.js
+++ frontend/src/components/ServiceOrderList.js
@@ -73,7 +73,7 @@ const ServiceRegisterList = ({history}) => {
                     <Row style={{marginBottom:'20px'}}>
                     {serviceOrderList.map((service) => (
                     <Col key={service._id} xs={12} sm={6} md={6} lg={4} xl={3} >
-                        <Card style={{margin:'5px',marginLeft:'40px',border:'1px solid #0B8A55',color:'white'}}>
+                        <Card style={{margin:'5px',marginLeft:'30px',border:'1px solid #0B8A55',color:'white'}}>
                             
                             <Card.Body>
                                 <Card.Title className="text-center font-weight-bold text-light bg-dark">{service.name}</Card.Title>
