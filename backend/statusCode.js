673e1e077eccc6e7057f1ee2bcf0caf3b7913463
Tushar Mazhar Talukdar <47280166+TusharMazhar@users.noreply.github.com>
2021-10-31 13:39:06 +0600

    statuscode added

---
+++ backend/statusCode.js
@@ -0,0 +1,15 @@
+ let statusCode = {
+
+    ok:200,
+    created:201,
+    movededPermanently:301,
+    found:302,
+    badRequest:400,
+    unAuthorized:401,
+    forbidden:403,
+    notFound:404,
+    methodNotAllowed:405,
+    toManyRequest:429,
+    internalServerError:500,
+
+}
